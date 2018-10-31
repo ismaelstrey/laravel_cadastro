9dea7f486983be3c125561922e7af8b4c606327f
ismael strey pereira <ismaelstrey@hotmail.com>
2018-10-30 23:36:38 -0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.app')
+@section('body')
+  <div class="jumbotron bg-light border border-secondary">
+    <div class="row">
+        .<div class="card-deck">
+            <div class="card border border-primary">
+                <div class="card-body">
+                    <h4 class="card-title">Cadastro de produtos</h4>
+                    <p class="card-text">Aqui vc cadastra todos os seu produtos.
+
+                        so não se esqueça de cadastrar previamente suas ctegorias.
+                    </p>
+                    <a href="/produtos" class="btn btn-primary">Cadastre seus produtos</a>
+                </div>
+            </div>
+            <div class="card border border-primary">
+                    <div class="card-body">
+                        <h4 class="card-title">Cadastro de categorias</h4>
+                        <p class="card-text">Aqui vc cadastra suas categorias.
+                        </p>
+                        <a href="/produtos" class="btn btn-secondary">Cadastre categorias</a>
+                    </div>
+                </div>
+        </div>
+    </div>
+  </div>
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Cadastro de Produtos</title>
+</head>
+<body>
+<div class="container">
+    @component('componente.componente_navbar')
+
+    @endcomponent
+    <main role="main">
+        @hasSection ('body')
+            @yield('body')
+        @endif
+    </main>
+</div>
+<script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
